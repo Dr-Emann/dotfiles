fb411527d9afeec6e8656736b2e12cfc53ebb35e
Zachary Dremann <dremann@gmail.com>
2014-01-16 19:12:50 -0500

    ensure submodules are grabbed when installing

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -22,6 +22,9 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
+git submodule init
+git submodule update
+
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
