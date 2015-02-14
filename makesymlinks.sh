4bbd39a474fa9b7ca4c9870b752a296d8b7a2d0c
Zachary Dremann <dremann@gmail.com>
2015-02-13 19:09:24 -0500

    smarter makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -12,10 +12,12 @@ files="bashrc dircolors vimrc vim gitconfig tmux.conf minttyrc oh-my-zsh zshrc" 
 
 ##########
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
+if ! [ -d "$olddir" ]; then
+    # create dotfiles_old in homedir
+    echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+    mkdir -p $olddir
+    echo "done"
+fi
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
@@ -32,9 +34,13 @@ ln -s $dir/dircolors-solarized/dircolors.ansi-dark $dir/dircolors
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    if [ -e "$olddir/$file" ]; then
+        echo "$olddir/$file exists. It will be backed up to \`$olddir/$file.old\`. If this script is run again, it will be LOST"
+        mv "$olddir/$file" "$olddir/$file.old"
+    fi
+    mv "~/.$file" "$olddir/$file"
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s "$dir/$file" "~/.$file"
 done
 
 # make vim folders
