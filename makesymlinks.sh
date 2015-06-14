468bad253e63f2629f7882b09cfe44d44ffbc3ac
Zachary Dremann <dremann@gmail.com>
2015-06-13 20:59:53 -0700

    Don't mess up if $HOME contains spaces

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,8 +6,8 @@
 
 ########## Variables
 
-dir=$HOME/dotfiles                    # dotfiles directory
-olddir=$HOME/dotfiles_old             # old dotfiles backup directory
+dir="$HOME/dotfiles"                    # dotfiles directory
+olddir="$HOME/dotfiles_old"             # old dotfiles backup directory
 files="bashrc dircolors vimrc vim gitconfig tmux.conf minttyrc oh-my-zsh zshrc"    # list of files/folders to symlink in homedir
 
 ##########
@@ -15,21 +15,21 @@ files="bashrc dircolors vimrc vim gitconfig tmux.conf minttyrc oh-my-zsh zshrc" 
 if ! [ -d "$olddir" ]; then
     # create dotfiles_old in homedir
     echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-    mkdir -p $olddir
+    mkdir -p "$olddir"
     echo "done"
 fi
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
-cd $dir
+cd "$dir"
 echo "done"
 
 git submodule init
 git submodule update
 
-$dir/gnome-terminal-colors-solarized/set_dark.sh
+"$dir/gnome-terminal-colors-solarized/set_dark.sh"
 
-ln -s $dir/dircolors-solarized/dircolors.ansi-dark $dir/dircolors
+ln -s "$dir/dircolors-solarized/dircolors.ansi-dark" "$dir/dircolors"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 echo "Moving any existing dotfiles from ~ to $olddir"
@@ -46,7 +46,7 @@ for file in $files; do
 done
 
 # make vim folders
-mkdir $HOME/.vim/backup
-mkdir $HOME/.vim/swap
-mkdir $HOME/.vim/undo
+mkdir "$HOME/.vim/backup"
+mkdir "$HOME/.vim/swap"
+mkdir "$HOME/.vim/undo"
 
